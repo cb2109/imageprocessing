2293cdff9fce19cad3bfeb66737d0365f4f8776f
Chris Bates <blokeyking@gmail.com>
2018-02-12 14:32:34 -0500

    Added edge hysterisis to filter out spurious "weak" edges. Tuned the canny edge detector default parameters to get good output for my current test images

--- src/main/java/user/cb2109/imageprocessing/imageprocessing/intensitytransformations/CannyEdgeDetectorTransformation.java
+++ src/main/java/user/cb2109/imageprocessing/imageprocessing/intensitytransformations/CannyEdgeDetectorTransformation.java
@@ -2,18 +2,24 @@ package user.cb2109.imageprocessing.imageprocessing.intensitytransformations;
 
 import user.cb2109.imageprocessing.imageprocessing.IntensityTransformation;
 
+import java.awt.*;
+import java.util.*;
+
 /**
  * Author: Christopher Bates
  * Date: 08/02/2018
  */
 public class CannyEdgeDetectorTransformation implements IntensityTransformation {
 
+    private static final int STRONG_PIXEL = 2;
+    private static final int WEAK_PIXEL = 1;
+
     private final GaussianSmoothingTransformation gaussianFilter;
     private final double highFilterThreshold;
     private final double lowFilterThreshold;
 
     public CannyEdgeDetectorTransformation() {
-        this(3, 1d, 255, 100);
+        this(3, 1d, 250, 50);
     }
 
     public CannyEdgeDetectorTransformation(int gaussianKernelSize, double gaussianStandardDeviation, double highFilterThreshold, double lowFilterThreshold) {
@@ -36,11 +42,6 @@ public class CannyEdgeDetectorTransformation implements IntensityTransformation 
             { 0,  0,  0},
             {-1, -2, -1}
     };
-
-    /*
-     * Not all kernels have the same idea of North for the angles
-     */
-    private final int kernelAngleAdjustment = 90;
 
     @Override
     public double[][] transform(double[][] image) {
@@ -75,16 +76,23 @@ public class CannyEdgeDetectorTransformation implements IntensityTransformation 
                 double intensity1 = convolutionX[x][y];
                 double intensity2 = convolutionY[x][y];
                 // how strong the edge is in the image
-                intensityMap[x][y] = Math.sqrt(intensity1 * intensity1 + intensity2 * intensity2);
+                intensityMap[x][y] = Math.hypot(intensity1, intensity2);
             }
         }
 
         double[][] filteredIntensityMap = computeIntensityDirectionality(convolutionX, convolutionY, intensityMap);
 
-        return markStrongWeakPixels(filteredIntensityMap);
+        double[][] strongWeakPixels =  markStrongWeakPixels(filteredIntensityMap);
+
+//        return strongWeakPixels;
+        return filterWeakPixels(strongWeakPixels);
 
     }
 
+    /*
+     * Remove noise by using the direction of the edge in the each pixel and the surrounding intensities to work out
+     * if the pixel edge is supported
+     */
     private double[][] computeIntensityDirectionality(double[][] convolutionX, double[][] convolutionY, double[][] intensityMap) {
         int imgWidth = intensityMap.length;
         int imgHeight = intensityMap[0].length;
@@ -128,8 +136,6 @@ public class CannyEdgeDetectorTransformation implements IntensityTransformation 
                 double ratioAngle = (edgeAngleRads * 4) / Math.PI;
                 // rounded to an int so we can talk about our "canonical" directions of -180 -135 ... 135 180
                 long roundedAngle = Math.round(ratioAngle) * 180 / 4;
-                // not all kernels compute angle in the same direction
-//                roundedAngle += this.kernelAngleAdjustment;
                 // ensure all lines only have 1 valid direction (e.g. -135 = 45 for a line)
                 long normalizedAngle = (roundedAngle + 180) % 180;
 
@@ -171,6 +177,11 @@ public class CannyEdgeDetectorTransformation implements IntensityTransformation 
         return output;
     }
 
+    /*
+     * Use a high-low filter to classify the edges into two groups
+     * - definitely edges
+     * - maybe edges
+     */
     private double[][] markStrongWeakPixels(double[][] filteredIntensityMap) {
         int imgWidth = filteredIntensityMap.length;
         int imgHeight = filteredIntensityMap[0].length;
@@ -185,14 +196,96 @@ public class CannyEdgeDetectorTransformation implements IntensityTransformation 
             for (int y = 0; y < imgHeight; y++) {
                 double currentIntensity = filteredIntensityMap[x][y];
                 if (currentIntensity >= highFilterThreshold) {
-                    output[x][y] = 2;
+                    output[x][y] = STRONG_PIXEL;
                 } else if (currentIntensity >= lowFilterThreshold) {
-                    output[x][y] = 1;
+                    output[x][y] = WEAK_PIXEL;
                 } else {
                     output[x][y] = 0;
                 }
             }
         }
         return output;
+    }
+
+    /*
+     * Use edge hysterisis with blob analysis to see if the weak edges are connected to strong edges
+     */
+    private double[][] filterWeakPixels(double[][] strongWeakPixels) {
+        int imgWidth = strongWeakPixels.length;
+        int imgHeight = strongWeakPixels[0].length;
+
+        // use this array to track groups of neighbouring edge pixels
+        double[][] labelArr = new double[imgWidth][imgHeight];
+        // track which labels contain strong pixels
+        Set<Integer> strongLabels = new LinkedHashSet<>();
+
+        int currentLabel = 1;
+        for (int x = 0; x < imgWidth; x++) {
+            for (int y = 0; y < imgHeight; y++) {
+                int pixelType = (int) strongWeakPixels[x][y];
+                double pixelLabel = labelArr[x][y];
+
+                switch (pixelType) {
+                    case STRONG_PIXEL:
+                        // add it to the array of labels that contain strong pixels
+                        if (pixelLabel > 0) {
+                            strongLabels.add((int) pixelLabel);
+                        } else {
+                            strongLabels.add(currentLabel);
+                        }
+                    case WEAK_PIXEL:
+                        // if this pixel is not already labelled
+                        if (pixelLabel == 0) {
+                            // label this area with the current label
+                            labelArr[x][y] = currentLabel;
+                            checkSurroundingPoints(strongWeakPixels, labelArr, currentLabel, new Point(x, y));
+                            currentLabel++;
+                        }
+                        break;
+                }
+
+            }
+        }
+
+        for (int x = 0; x < imgWidth; x++) {
+            for (int y = 0; y < imgHeight; y++) {
+                int label = (int) labelArr[x][y];
+                // if the pixels labeled by this label contained a strong edge then it is a valid edge
+                if (label > 0 && strongLabels.contains(label)) {
+                    labelArr[x][y] = 1;
+                } else {
+                    labelArr[x][y] = 0;
+                }
+            }
+        }
+        return labelArr;
+    }
+
+    private void checkSurroundingPoints(double[][] strongWeakPixels, double[][] labelArr, int currentLabel, Point start) {
+        Queue<Point> pointsToCheck = new ArrayDeque<>();
+        pointsToCheck.add(start);
+        while (!pointsToCheck.isEmpty()) {
+            Point current = pointsToCheck.remove();
+            int x = current.x;
+            int y = current.y;
+            // check surrounding points
+            for (int i = -1; i <= 1; i++) {
+                for (int j = -1; j <= 1; j++) {
+                    int xi = x + i;
+                    int yj = y + j;
+                    // boundary check
+                    if (xi < 0 || yj  < 0 || xi > strongWeakPixels.length || yj > strongWeakPixels[0].length) {
+                        continue;
+                    }
+                    double pixelStrength = strongWeakPixels[xi][yj];
+                    double pixelLabel = labelArr[xi][yj];
+                    // only mark the point if it isn't already marked AND add it to be boundary checked
+                    if (pixelStrength > 0 && pixelLabel == 0) {
+                        labelArr[xi][yj] = currentLabel;
+                        pointsToCheck.add(new Point(xi, yj));
+                    }
+                }
+            }
+        }
     }
 }
